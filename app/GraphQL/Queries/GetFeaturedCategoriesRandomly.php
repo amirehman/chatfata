997b743e4153db3095257454aac583333c1f9f10
amirehman <aamirnorth@gmail.com>
2020-05-28 08:56:29 +0400

    featured category query changed

--- app/GraphQL/Queries/GetFeaturedCategoriesRandomly.php
+++ app/GraphQL/Queries/GetFeaturedCategoriesRandomly.php
@@ -18,6 +18,10 @@ class GetFeaturedCategoriesRandomly
      */
     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
-        return \App\Ingredient::where('category', '=', 1)->get()->random(10);
+        // return \App\Ingredient::where('category', '=', 1)->get()->random(10);
+
+        return \App\Ingredient::whereHas('recipes', function ($query) {
+            $query->where('status', '=', 'PUBLISHED');
+        })->get();
     }
 }

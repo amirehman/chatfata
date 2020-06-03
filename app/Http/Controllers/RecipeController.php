c73f22dce94ae35e62c0094226f0481c63249859
amirehman <aamirnorth@gmail.com>
2020-06-03 16:27:54 +0400

    note added to updated recipe

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -206,6 +206,7 @@ class RecipeController extends Controller
         $recipe->body = $request->detail;
         $recipe->prep_time = $request->prep_time;
         $recipe->video = $request->video;
+        $recipe->note = $request->note;
 
         $recipe->difficulty = $request->difficulty;
 
